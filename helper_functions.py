0534e03d7fdf119488c4f4e4126f3c3ceac49328
Franz0808 <97342392+Franz0808@users.noreply.github.com>
2023-12-12 16:42:15 +0100

    Initial commit

---
+++ helper_functions.py
@@ -0,0 +1,46 @@
+def jukes_cantor(reference_sequence: str, distant_sequence: str) -> float:
+    """The Jukes-Cantor correction for estimating genetic distances
+    calculated with Hamming distance.
+    Should return genetic distance with the same unit as if not corrected.
+
+    Parameters
+    ----------
+    referene_sequence: str
+        A string of nucleotides in a sequence used as a reference
+        in an alignment with other (e.g. AGGT-GA)
+    distant_sequence: str
+        A string of nucleotides in a sequence after the alignment
+        with a reference (e.g. AGC-AGA)
+
+    Returns
+    -------
+    float
+        The Jukes-Cantor corrected genetic distance using Hamming distance.
+        For example 1.163.
+
+    """
+    raise NotImplementedError()
+
+
+def kimura_two_parameter(reference_sequence: str, distant_sequence: str) -> float:
+    """The Kimura Two Parameter correction for estimating genetic distances
+    calculated with Hamming distance.
+    Should return genetic distance with the same unit as if not corrected.
+
+    Parameters
+    ----------
+    referene_sequence: str
+        A string of nucleotides in a sequence used as a reference
+        in an alignment with other (e.g. AGGT-GA)
+    distant_sequence: str
+        A string of nucleotides in a sequence after the alignment
+        with a reference (e.g. AGC-AGA)
+
+    Returns
+    -------
+    float
+        The Kimura corrected genetic distance using Hamming distance.
+        For example 1.196.
+
+    """
+    raise NotImplementedError()
